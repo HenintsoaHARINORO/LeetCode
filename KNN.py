743fb22c013d9613442fcfc007dc388e43258c5d
HenintsoaHARINORO <henintsoaharinororandrianasolo@gmail.com>
2023-09-13 09:11:30 +0300

    KNN

---
+++ KNN.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        abs_diff = [(abs(num - x), num) for num in arr].sort()
+        nearest_neighbors = abs_diff[:k]
+        result = sorted([neighbor[1] for neighbor in nearest_neighbors])
+
+        return result
+
+
+# Example usage
+arr = [1, 2, 3, 4, 5]
+k = 4
+x = 3
+
+solution = Solution()
+result = solution.findClosestElements(arr, k, x)
+
+print(" Nearest Neighbors:", result)
